56e4f861efa5432b27ee39b628cc5d6b253af704
BELLARMINE\aruf <aruf@bellarmine.edu>
2017-12-01 13:15:02 -0500

    Added CSharp versiobn

---
+++ CSharp/ConsoleApp1/LinqHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    static class LinqHelpers
+    {
+        public static IEnumerable<T> Cycle<T>(this IList<T> xs)
+        {
+            int i = 0;
+            while (true)
+            {
+                int oldI = i;
+                if (i == xs.Count)
+                {
+                    i = 1;
+                    yield return xs[0];
+                }
+                else
+                {
+                    i++;
+                    yield return xs[oldI];
+                }
+            }
+        }
+    }
+}
